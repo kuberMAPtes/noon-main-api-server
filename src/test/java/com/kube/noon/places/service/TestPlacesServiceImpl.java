3b2bb83ece0c8dbd2874c73301af3317980d3055
pgd <rudeh1253@gmail.com>
2024-06-01 12:38:38 +0900

    test: TestPlacesServiceImpl에 boundary case 추가
    장소명으로 검색: 존재하지 않는 장소를 검색할 경우 빈 List 반환
    위도, 경도로 검색: 한국 범위를 벗어나는 장소를 검색할 경우
    PlaceNotFoundException 발행
    
    Related to: #14

--- src/test/java/com/kube/noon/places/service/TestPlacesServiceImpl.java
+++ src/test/java/com/kube/noon/places/service/TestPlacesServiceImpl.java
@@ -5,6 +5,9 @@ import com.kube.noon.places.exception.PlaceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +15,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @Slf4j
 @SpringBootTest
@@ -32,6 +36,19 @@ class TestPlacesServiceImpl {
             log.info("위도={}", result.getX());
             log.info("경도={}", result.getY());
         }
+    }
+
+    @DisplayName("장소명으로 검색 - 존재하지 않는 장소를 검색할 경우 빈 List 반환")
+    @ValueSource(strings = {
+            "avsipndgpowipghrwpibjzx",
+            "이런장소는이세상에없겠지전혀없겠지검색결과아예없겠지",
+            "avshfibhpasfhb9q4htphwep",
+            "이런장소가있다면 내 손에 장을 지진다."
+    })
+    @ParameterizedTest
+    void getPlaceList_searchPlaceNotExists(String testCase) {
+        List<PlaceDto> result = this.placesService.getPlaceList(testCase);
+        assertThat(result).isEmpty();
     }
 
     @DisplayName("위도, 경도로 검색")
@@ -64,5 +81,17 @@ class TestPlacesServiceImpl {
 
         // 세 번째 좌표는 다른 건물
         assertThat(results[2].getRoadAddress()).isNotEqualTo(results[0].getRoadAddress());
+    }
+
+    @DisplayName("위도, 경도로 검색 - 유효하지 않은 위도, 경도")
+    @CsvSource(value = {
+            "1.4215314,128.512421583",
+            "35.124152424,13.12584814",
+            "12.2442513,16.3858142"
+    }, delimiter = ',')
+    @ParameterizedTest
+    void getPlaceByLatLng_한국에_존재하지_않는_위도_및_경도(double latitude, double longitude) {
+        assertThatExceptionOfType(PlaceNotFoundException.class)
+                .isThrownBy(() -> this.placesService.getPlaceByLatLng(latitude, longitude));
     }
 }
