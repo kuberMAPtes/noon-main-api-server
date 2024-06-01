8b6e77d0c79138c265d8a7728303548bbbbb0ecd
pgd <rudeh1253@gmail.com>
2024-06-01 12:27:55 +0900

    test: TestPlacesServiceImpl 테스트 코드 변경
    Related to: #10, #14

--- src/test/java/com/kube/noon/places/repository/TestPlacesNaverMapsApiRepository.java
+++ src/test/java/com/kube/noon/places/repository/TestPlacesNaverMapsApiRepository.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.kube.noon.places.domain.Place;
 import com.kube.noon.places.exception.PlaceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +21,12 @@ class TestPlacesNaverMapsApiRepository {
     @Autowired
     PlacesNaverMapsApiRepository placesRepository;
 
-    @DisplayName("장소명으로 도로명주소, 위도, 경도 가져오기 테스트 - log만 확인")
+    @DisplayName("장소명으로 도로명주소, 위도, 경도 가져오기 테스트")
     @Test
     void findByPlaceName() {
-        List<Place> results = this.placesRepository.findByPlaceName("비트캠프");
+        List<Place> results = this.placesRepository.findByPlaceName("강남");
+
+        assertThat(results).isNotEmpty();
 
         for (Place result : results) {
             log.info("도로명주소={}", result.getRoadAddress());
@@ -32,7 +35,7 @@ class TestPlacesNaverMapsApiRepository {
         }
     }
 
-    @DisplayName("위도, 경도 좌표로 도로명주소, 위도, 경도 가져오기 테스트 - log만 확인")
+    @DisplayName("위도, 경도 좌표로 도로명주소, 위도, 경도 가져오기 테스트")
     @Test
     void findByLatLng() {
         final int latIdx = 0;
