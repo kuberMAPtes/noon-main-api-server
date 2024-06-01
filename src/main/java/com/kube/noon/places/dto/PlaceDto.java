3deca1b54db2f044776d4c6e57fd1e14313e48e1
pgd <rudeh1253@gmail.com>
2024-06-01 12:02:46 +0900

    feat: 장소 데이터를 담는 DTO 객체 정의
    Related to: #14

--- src/main/java/com/kube/noon/places/dto/PlaceDto.java
+++ src/main/java/com/kube/noon/places/dto/PlaceDto.java
@@ -1,4 +1,29 @@
 package com.kube.noon.places.dto;
 
+import com.kube.noon.places.domain.Place;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@ToString
 public class PlaceDto {
+    private String placeName;
+    private String roadAddress;
+    private double x;
+    private double y;
+
+    private PlaceDto() {
+    }
+
+    public static PlaceDto from(Place place) {
+        return PlaceDto.builder()
+                .placeName(place.getPlaceName())
+                .roadAddress(place.getRoadAddress())
+                .x(place.getX())
+                .y(place.getY())
+                .build();
+    }
 }
