b370b17ba4552e904918d8adef0945b94f6c0232
ellen310 <ellen10735@gmail.com>
2024-06-01 14:05:40 +0900

    feat : Building 도메인 객체 정의 및 BuildingProfileRepositor 및 구현

--- src/main/java/com/kube/noon/building/repository/BuildingProfileRepository.java
+++ src/main/java/com/kube/noon/building/repository/BuildingProfileRepository.java
@@ -1,4 +1,12 @@
 package com.kube.noon.building.repository;
 
-public interface BuildingProfileRepository {
+import com.kube.noon.building.domain.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BuildingProfileRepository extends JpaRepository<Building, Integer> {
+
+    Building findBuildingProfileByBuildingId(int buildingId);
+
 }
