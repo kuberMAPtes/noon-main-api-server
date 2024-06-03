9bba5da9166c0d9c6ac52be25fae49f72186caf4
pgd <rudeh1253@gmail.com>
2024-06-03 19:01:30 +0900

    test: (TestBuildingWikiRestTemplateServiceImpl) 테스트 코드 보강
    profile에 prod가 없는 경우 BuildingWikiRestTemplateServiceImpl이 스프링
    빈으로 등록되지 않아서 테스트가 실패한다. 이점 해결
    
    Related to: #19

--- src/test/java/com/kube/noon/building/service/TestBuildingWikiRestTemplateServiceImpl.java
+++ src/test/java/com/kube/noon/building/service/TestBuildingWikiRestTemplateServiceImpl.java
@@ -1,16 +1,33 @@
 package com.kube.noon.building.service;
 
 import com.kube.noon.building.service.buildingwiki.BuildingWikiRestTemplateServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 class TestBuildingWikiRestTemplateServiceImpl {
 
-    @Autowired
     BuildingWikiRestTemplateServiceImpl wikiService;
+
+    static Properties properties = new Properties();
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        try (InputStream inputStream = TestBuildingWikiRestTemplateServiceImpl.class
+                .getResourceAsStream("/application-key.properties")) {
+            properties.load(inputStream);
+        }
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        this.wikiService = new BuildingWikiRestTemplateServiceImpl(properties.getProperty("building-wiki-url"));
+    }
 
     @DisplayName("직접 위키 페이지 확인해 보면서 테스트")
     @Test
