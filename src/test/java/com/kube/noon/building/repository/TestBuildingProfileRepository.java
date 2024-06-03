2ee686e0ee2f1356493a30e626b8a1bf7cb48cf3
ellen310 <ellen10735@gmail.com>
2024-06-03 15:20:46 +0900

    feat : BuildingProfile관련 Repository, Service Test Code 작성

--- src/test/java/com/kube/noon/building/repository/TestBuildingProfileRepository.java
+++ src/test/java/com/kube/noon/building/repository/TestBuildingProfileRepository.java
@@ -1,7 +1,10 @@
 package com.kube.noon.building.repository;
 
 import com.kube.noon.building.domain.Building;
+import com.kube.noon.building.dto.BuildingDto;
+import com.kube.noon.building.repository.mapper.BuildingProfileMapper;
 import com.kube.noon.common.zzim.ZzimRepository;
+import com.kube.noon.member.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @SpringBootTest
@@ -17,8 +21,10 @@ class TestBuildingProfileRepository {
     @Autowired
     BuildingProfileRepository buildingProfileRepository;
 
+
     @Autowired
-    ZzimRepository zzimRepository;
+    BuildingProfileMapper buildingProfileMapper;
+
 
     @DisplayName("빌딩아이디로 빌딩 정보가져오기")
     @Test
@@ -34,18 +40,42 @@ class TestBuildingProfileRepository {
     }
 
     @DisplayName("멤버아이디로 회원별 구독 건물 정보가져오기")
-    //@Test
+    @Test
     void findUserBuildingSubscriptionListByMemberId() {
-        List<Building> results = zzimRepository.findBuildingSubscriptionListByMemberId("member_996");
 
-        for (Building result : results) {
+        List<Building> buildings = buildingProfileMapper.findBuildingSubscriptionListByMemberId("member_996");
+        List<BuildingDto> buildingDtos = buildings.stream()
+                .map(BuildingDto::fromEntity)
+                .collect(Collectors.toList());
 
-            log.info("빌딩이름={}", result.getBuildingName());
-            log.info("빌딩경도={}", result.getLongitude());
-            log.info("빌딩위도={}", result.getLatitude());
-            log.info("빌딩피드요약={}", result.getFeedAiSummary());
-            log.info("빌딩도로명주소={}", result.getRoadAddr());
+        for (BuildingDto buildingDto : buildingDtos) {
 
+            log.info("빌딩이름={}", buildingDto.getBuildingName());
+            log.info("빌딩경도={}", buildingDto.getLongitude());
+            log.info("빌딩위도={}", buildingDto.getLatitude());
+            log.info("빌딩피드요약={}", buildingDto.getFeedAiSummary());
+            log.info("빌딩도로명주소={}", buildingDto.getRoadAddr());
+
+
+        }
+
+    }
+
+    @DisplayName("건물아이디로 건물의 구독자 목록 가져오기")
+    @Test
+    void findBuildingSubscriberListByBuildingId() {
+
+        List<Member> members = buildingProfileMapper.findBuildingSubscriberListByBuildingId(11513);
+
+        for (Member member : members) {
+
+            log.info("회원아이디={}", member.getMemberId());
+            log.info("회원비밀번호={}", member.getPassword());
+            log.info("프로필사진경로={}", member.getProfilePhotoUrl());
+            log.info("닉네임={}", member.getNickname());
+            log.info("다정수치={}", member.getDajungScore());
+            log.info("전화번호={}", member.getPhoneNumber());
+            log.info("프로필소개={}", member.getProfileIntro());
 
         }
 
--- src/test/java/com/kube/noon/building/service/TestBuildingService.java
+++ src/test/java/com/kube/noon/building/service/TestBuildingService.java
@@ -1,11 +1,50 @@
 package com.kube.noon.building.service;
+import com.kube.noon.building.domain.Building;
+import com.kube.noon.building.repository.mapper.BuildingProfileMapper;
+import com.kube.noon.common.zzim.Zzim;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 public class TestBuildingService {
 
+    @Autowired
+    @Qualifier("buildingProfileServiceImpl")
+    BuildingProfileService buildingProfileService;
+
+
+    @DisplayName("멤버아이디, 건물아이디로 구독하기")
     @Test
-    void contextLoads() {
+    void addSubscription() {
+
+        Zzim zzim = buildingProfileService.addSubscription("member_2",10000);
+
+        log.info("건물아이디={}", zzim.getBuildingId());
+        log.info("찜타입={}", zzim.getZzimType());
+        log.info("구독자아이디={}", zzim.getMemberId());
+        log.info("구독제공자아이디={}", zzim.getSubscriptionProviderId());
+        log.info("찜아이디={}", zzim.getZzimId());
+        log.info("찜activated={}", zzim.isActivated());
+
+    }
+
+    @DisplayName("멤버아이디, 건물아이디로 구독 취소하기")
+    @Test
+    void deleteSubscription() {
+
+        Zzim zzim = buildingProfileService.deleteSubscription("member_2",10000);
+
+        log.info("건물아이디={}", zzim.getBuildingId());
+        log.info("찜타입={}", zzim.getZzimType());
+        log.info("구독자아이디={}", zzim.getMemberId());
+        log.info("구독제공자아이디={}", zzim.getSubscriptionProviderId());
+        log.info("찜아이디={}", zzim.getZzimId());
+        log.info("찜activated={}", zzim.isActivated());
+
     }
 }
