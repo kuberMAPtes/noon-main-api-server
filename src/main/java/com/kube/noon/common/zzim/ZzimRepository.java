ec9e0a9d22f9095db7a3aa3dec374bbc7a4f85d3
ellen310 <ellen10735@gmail.com>
2024-06-03 15:20:16 +0900

    feat : ZzimRepository 정의 및 구독 히스토리조회, 구독정보 업데이트, Zzim레코드 조회 구현

--- src/main/java/com/kube/noon/common/zzim/ZzimRepository.java
+++ src/main/java/com/kube/noon/common/zzim/ZzimRepository.java
@@ -1,33 +1,52 @@
 package com.kube.noon.common.zzim;
 
-import com.kube.noon.building.domain.Building;
+import jakarta.transaction.Transactional;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 
 
 @Repository
 public interface ZzimRepository extends JpaRepository<Zzim, Integer> {
 
     /**
-     * 특정 사용자 ID로 건물 구독 목록을 찾는다.
+     * 본인이 건물을 구독했던 History가 있는지 조회.
+     * History가 있다면 재구독 할 때 activated만 변경한다.
+     * History가 없다면 새 레코드를 INSERT한다.
      *
-     * @param memberId 본인의 구독 목록을 보려는 회원의 계정 ID
-     * @return 구독한 건물 목록
+     * @param buildingId 구독History를 조회하려는 건물 아이디
+     * @param memberId 구독History를 조회하려는 회원 아이디
+     * @param subscriptionProviderId 본인이 구독했었는지, 타회원 구독 가져오기로 구독했었는지 구분하는 필드
+     * @return 구독했던 이력이 있다면 true, 없다면 false
+     *
+     * @author 허예지
+     *
+     */
+    boolean existsByBuildingIdAndMemberIdAndSubscriptionProviderId(int buildingId, String memberId, String subscriptionProviderId);
+
+
+    /**
+     * 회원이 구독했다 취소한 History가 있다면 그 레코드의 activate만 true로 update한다.
+     *
+     * @param activated true이면 구독, false이면 구독취소
      *
      * @author 허예지
      */
-    @Query(value = """
-            SELECT *
-            FROM building
-            WHERE building_id IN (SELECT building_id
-                    FROM zzim
-                    WHERE zzim_type='SUBSCRIPTION' AND member_id = :memberId AND activated = 1)
-            """, nativeQuery = true)
-    public List<Building> findBuildingSubscriptionListByMemberId(@Param("memberId") String memberId);
+    @Modifying
+    @Transactional
+    @Query("UPDATE Zzim SET activated = :activated WHERE buildingId = :buildingId AND memberId = :memberId")
+    void updateActivatedByBuildingIdAndMemberIdAndSubscriptionProviderId(@Param("buildingId") int buildingId,
+                                                                         @Param("memberId") String memberId,
+                                                                         @Param("activated") boolean activated);
 
+    /**
+     * 빌딩아이디, 구독자아이디, 구독제공자아이디로 Zzim레코드를 조회한다.
+     * 
+     * @return Zzim엔티티
+     *
+     * @author 허예지
+     */
+    Zzim findByBuildingIdAndMemberId(int buildingId, String memberId);
 }
