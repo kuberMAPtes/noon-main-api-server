fb020f71bf1658c3a57dce8541246a08634ce64e
ellen310 <ellen10735@gmail.com>
2024-06-03 15:17:23 +0900

    feat : 빌딩 프로필 가져오기 Repository 구현

--- src/main/java/com/kube/noon/building/repository/BuildingProfileRepository.java
+++ src/main/java/com/kube/noon/building/repository/BuildingProfileRepository.java
@@ -7,7 +7,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BuildingProfileRepository extends JpaRepository<Building, Integer> {
 
+
+    /**
+     * 건물 프로필 보기
+     * Building ID로 특정 건물의 정보를 가져온다.
+     *
+     * @param buildingId 건물 정보를 보고자 하는 건물의 ID
+     * @return Building 정보
+     *
+     * @author 허예지
+     */
     Building findBuildingProfileByBuildingId(int buildingId);
+
+
 
 
 }
