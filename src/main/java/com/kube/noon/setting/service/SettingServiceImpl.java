8a658ebf4fbc8647b1a14456a85efb4000f720b2
pgd <rudeh1253@gmail.com>
2024-06-03 19:56:36 +0900

    feat: SettingServiceImpl 구현

--- src/main/java/com/kube/noon/setting/service/SettingServiceImpl.java
+++ src/main/java/com/kube/noon/setting/service/SettingServiceImpl.java
@@ -1,5 +1,9 @@
 package com.kube.noon.setting.service;
 
+import com.kube.noon.member.binder.MemberBinder;
+import com.kube.noon.member.domain.Member;
+import com.kube.noon.member.dto.UpdateMemberDto;
+import com.kube.noon.member.service.MemberService;
 import com.kube.noon.setting.domain.Setting;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,16 +13,27 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class SettingServiceImpl implements SettingService {
+    private final MemberService memberService;
 
     @Override
     public void updateSetting(String memberId, Setting newSetting) {
-        // TODO
-        throw new UnsupportedOperationException();
+        Member findMember = this.memberService.findMemberById(memberId);
+        UpdateMemberDto dto = MemberBinder.INSTANCE.toDto(findMember, UpdateMemberDto.class);
+        dto.setMemberProfilePublicRange(newSetting.getMemberProfilePublicRange());
+        dto.setAllFeedPublicRange(newSetting.getAllFeedPublicRange());
+        dto.setBuildingSubscriptionPublicRange(newSetting.getBuildingSubscriptionPublicRange());
+        dto.setReceivingAllNotificationAllowed(newSetting.isReceivingAllNotificationAllowed());
+        this.memberService.updateMember(dto);
     }
 
     @Override
     public Setting findSettingOfMember(String memberId) {
-        // TODO
-        throw new UnsupportedOperationException();
+        Member findMember = this.memberService.findMemberById(memberId);
+        return Setting.builder()
+                .memberProfilePublicRange(findMember.getMemberProfilePublicRange())
+                .allFeedPublicRange(findMember.getAllFeedPublicRange())
+                .buildingSubscriptionPublicRange(findMember.getBuildingSubscriptionPublicRange())
+                .receivingAllNotificationAllowed(findMember.isReceivingAllNotificationAllowed())
+                .build();
     }
 }
