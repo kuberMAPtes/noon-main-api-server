fbe86ff6c7e4abc8c4d8a7b282864e11e2a2fb20
pgd <rudeh1253@gmail.com>
2024-06-03 17:18:12 +0900

    feat: RestTemplate 기반 BuildingWiki 구현
    Related to: #19

---
+++ src/main/java/com/kube/noon/building/service/BuildingWikiRestTemplateServiceImpl.java
@@ -0,0 +1,76 @@
+package com.kube.noon.building.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class BuildingWikiRestTemplateServiceImpl implements BuildingWikiService {
+    private final String buildingWikiUrl;
+    private final RestTemplate restTemplate;
+
+    public BuildingWikiRestTemplateServiceImpl(@Value("${building-wiki-url}") String buildingWikiUrl) {
+        this.buildingWikiUrl = buildingWikiUrl;
+        this.restTemplate = new RestTemplate();
+    }
+
+    @Override
+    public void addPage(String title) {
+        if (this.buildingWikiUrl == null) {
+            throw new IllegalStateException(this.getClass() + ".buildingWikiUrl is null");
+        }
+
+        if (isExist(title)) {
+            log.info("Building Wiki Page of title({}) already exists", title);
+            return;
+        }
+
+        log.info("Create Wiki Page of title: {}", title);
+        URI uri = UriComponentsBuilder.fromUri(URI.create(this.buildingWikiUrl))
+                .path("/wiki/api.php")
+                .queryParam("action", "edit")
+                .queryParam("title", title)
+                .queryParam("text", "")
+                .queryParam("format", "json")
+                .build().toUri();
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("token", "+\\");
+        RequestEntity<MultiValueMap<String, String>> requestEntity =
+                RequestEntity.post(uri)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(body);
+        this.restTemplate.exchange(requestEntity, String.class);
+    }
+
+    private boolean isExist(String title) {
+        URI uri = UriComponentsBuilder.fromUri(URI.create(this.buildingWikiUrl))
+                .path("/wiki/api.php")
+                .queryParam("action", "query")
+                .queryParam("titles", title)
+                .queryParam("format", "json")
+                .build()
+                .toUri();
+        RequestEntity<Void> requestEntity = RequestEntity.get(uri).build();
+        ResponseEntity<String> responseEntity = this.restTemplate.exchange(requestEntity, String.class);
+        JSONObject body = new JSONObject(responseEntity.getBody());
+        JSONObject pages = body.getJSONObject("query").getJSONObject("pages");
+        try {
+            pages.getJSONObject("-1");
+            return false;
+        } catch (JSONException e) {
+            return true;
+        }
+    }
+}
---
+++ src/test/java/com/kube/noon/building/service/TestBuildingWikiRestTemplateServiceImpl.java
@@ -0,0 +1,19 @@
+package com.kube.noon.building.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBuildingWikiRestTemplateServiceImpl {
+
+    @Autowired
+    BuildingWikiRestTemplateServiceImpl wikiService;
+
+    @DisplayName("직접 위키 페이지 확인해 보면서 테스트")
+    @Test
+    void addPage() {
+        wikiService.addPage("Sample-pageasdf");
+    }
+}
