ac0e846a589a14cb60b9a511cdacce2988e25d0c
ellen310 <ellen10735@gmail.com>
2024-06-03 15:18:27 +0900

    feat : 빌딩 구독 및 구독취소 Service 정의, ServiceImpl 구현

--- src/main/java/com/kube/noon/building/service/BuildingProfileService.java
+++ src/main/java/com/kube/noon/building/service/BuildingProfileService.java
@@ -2,12 +2,15 @@ package com.kube.noon.building.service;
 
 import com.kube.noon.building.domain.Building;
 import com.kube.noon.building.dto.BuildingDto;
+import com.kube.noon.common.zzim.Zzim;
 
 import java.util.List;
 
 public interface BuildingProfileService {
 
-    void manageSubscription(String userId, int buildingId, boolean subscribe);
+    Zzim addSubscription(String memberId, int buildingId);
+    Zzim deleteSubscription(String memberId, int buildingId);
     List<BuildingDto> getUserBuildingSubscriptionList(String memberId);
     Building getBuildingProfile(int buildingId);
+    List<BuildingDto> getBuildingSubscriptionListByMemberId(String memberId);
 }
--- src/main/java/com/kube/noon/building/service/BuildingProfileServiceImpl.java
+++ src/main/java/com/kube/noon/building/service/BuildingProfileServiceImpl.java
@@ -2,8 +2,12 @@ package com.kube.noon.building.service;
 
 import com.kube.noon.building.domain.Building;
 import com.kube.noon.building.dto.BuildingDto;
+import com.kube.noon.building.repository.mapper.BuildingProfileMapper;
 import com.kube.noon.building.repository.BuildingProfileRepository;
+import com.kube.noon.common.zzim.Zzim;
 import com.kube.noon.common.zzim.ZzimRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -19,37 +23,75 @@ import java.util.stream.Collectors;
 @Service("buildingProfileServiceImpl")
 public class BuildingProfileServiceImpl implements BuildingProfileService {
 
+    @Autowired
     @Qualifier("buildingProfileRepository")
     private BuildingProfileRepository buildingProfileRepository;
 
+    @Autowired
     @Qualifier("zzimRepository")
     private ZzimRepository zzimRepository;
 
+    @Autowired
+    @Qualifier("buildingProfileMapper")
+    private BuildingProfileMapper buildingProfileMapper;
 
 
 
+    /**
+     * 본인이 직접 건물을 구독하는 기능
+     * 구독했다 취소한 History가 있다면, 그 레코드의 activated만 수정한다.
+     *
+     * @param memberId 구독하려는 회원아이디
+     * @param buildingId 구독하려는 빌딩아이디
+     * @return
+     *
+     * @author 허예지
+     */
+    @Transactional
+    public Zzim addSubscription(String memberId, int buildingId) {
 
+        boolean historyExist = zzimRepository.existsByBuildingIdAndMemberIdAndSubscriptionProviderId(buildingId, memberId, memberId);
 
-    @Override
-    public void manageSubscription(String memberId, int buildingId, boolean subscribe) {
+        if(!historyExist){
+            Zzim zzim = Zzim.builder()
+                    .memberId(memberId)
+                    .feedId(null)
+                    .buildingId(buildingId)
+                    .subscriptionProviderId(memberId)
+                    .zzimType("SUBSCRIPTION")
+                    .activated(true)
+                    .build();
 
-        /*
-        List<String> subscriptions = userSubscriptions.getOrDefault(userId, new ArrayList<>());
-        if (subscribe) {
-            if (!subscriptions.contains(buildingId)) {
-                buildingProfileRepository.save();
-            }
-        } else {
-            subscriptions.remove(buildingId);
+            return zzimRepository.save(zzim);
+
+        }else{
+
+            zzimRepository.updateActivatedByBuildingIdAndMemberIdAndSubscriptionProviderId(buildingId, memberId, true);
+            return zzimRepository.findByBuildingIdAndMemberId(buildingId, memberId);
+
         }
-        userSubscriptions.put(userId, subscriptions);
-
-         */
     }
+
+    /**
+     * 특정 건물에 대한 구독을 취소한다.
+     * 본인이 직접 구독했는지, 건물합치기로 구독했는지는 구분하지 않는다. (한 건물에 대한 구독레코드는 어떤 방식으로든 1개이므로)
+     *
+     * @return activated가 false가 된 Zzim엔티티
+     *
+     * @author 허예지
+     */
+    public Zzim deleteSubscription(String memberId, int buildingId) {
+
+        zzimRepository.updateActivatedByBuildingIdAndMemberIdAndSubscriptionProviderId(buildingId, memberId, false);
+
+        return zzimRepository.findByBuildingIdAndMemberId(buildingId, memberId);
+    }
+
+
 
     @Override
     public List<BuildingDto> getUserBuildingSubscriptionList(String memberId) {
-        List<Building> buildings = zzimRepository.findBuildingSubscriptionListByMemberId(memberId);
+        List<Building> buildings = buildingProfileMapper.findBuildingSubscriptionListByMemberId(memberId);
 
         return buildings.stream()
                 .map(BuildingDto::fromEntity)
@@ -59,5 +101,13 @@ public class BuildingProfileServiceImpl implements BuildingProfileService {
     @Override
     public Building getBuildingProfile(int buildingId) {
         return buildingProfileRepository.findBuildingProfileByBuildingId(buildingId);
+    }
+
+    @Override
+    public List<BuildingDto> getBuildingSubscriptionListByMemberId(String memberId) {
+        List<Building> buildings = buildingProfileMapper.findBuildingSubscriptionListByMemberId(memberId);
+        return buildings.stream()
+                .map(BuildingDto::fromEntity)
+                .collect(Collectors.toList());
     }
 }
