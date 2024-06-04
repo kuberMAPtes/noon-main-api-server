743b5a83f4fe93d13bdb5ea9664aaff89d36e12a
ellen310 <ellen10735@gmail.com>
2024-06-04 10:48:47 +0900

    chore : 신고 관련 엔티티, Dto 정의

---
+++ src/main/java/com/kube/noon/customersupport/dto/report/ReportProcessingDto.java
@@ -0,0 +1,53 @@
+package com.kube.noon.customersupport.dto.report;
+
+import com.kube.noon.customersupport.domain.Report;
+import com.kube.noon.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 신고 처리 내용을 담는 Dto
+ *
+ */
+@Data
+@Builder
+public class ReportProcessingDto {
+
+    private Integer reportId;
+    private String reporterId;
+    private String reporteeId;
+    private Report.ReportStatus reportStatus;
+    private String reportText;
+    private LocalDateTime reportedTime;
+    private String processingText;
+
+
+    public static ReportProcessingDto fromEntity(Report report) {
+        return ReportProcessingDto.builder()
+                .reportId(report.getReportId())
+                .reporterId(report.getReporterId())
+                .reporteeId(report.getReporteeId())
+                .reportStatus(report.getReportStatus())
+                .reportText(report.getReportText())
+                .reportedTime(report.getReportedTime())
+                .processingText(report.getProcessingText())
+                .build();
+    }
+
+    public Report toEntity() {
+        return Report.builder()
+                .reportId(this.reportId)
+                .reporterId(this.reporterId)
+                .reporteeId(this.reporteeId)
+                .reportStatus(this.reportStatus)
+                .reportText(this.reportText)
+                .reportedTime(this.reportedTime)
+                .processingText(this.processingText)
+                .build();
+    }
+
+
+}
