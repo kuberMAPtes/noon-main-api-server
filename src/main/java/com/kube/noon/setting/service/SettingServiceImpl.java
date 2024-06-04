8fa75ab40d57f389b88e05a5cd8b0ec5f84c4b33
pgd <rudeh1253@gmail.com>
2024-06-04 10:23:25 +0900

    refactor: 환경설정 ServiceImpl 버그 수정
    테스트 코드도 추가
    
    Related to: #36

--- src/main/java/com/kube/noon/setting/service/SettingServiceImpl.java
+++ src/main/java/com/kube/noon/setting/service/SettingServiceImpl.java
@@ -1,6 +1,5 @@
 package com.kube.noon.setting.service;
 
-import com.kube.noon.member.binder.MemberBinder;
 import com.kube.noon.member.domain.Member;
 import com.kube.noon.member.dto.UpdateMemberDto;
 import com.kube.noon.member.service.MemberService;
@@ -8,6 +7,8 @@ import com.kube.noon.setting.domain.Setting;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -17,23 +18,25 @@ public class SettingServiceImpl implements SettingService {
 
     @Override
     public void updateSetting(String memberId, Setting newSetting) {
-        Member findMember = this.memberService.findMemberById(memberId);
-        UpdateMemberDto dto = MemberBinder.INSTANCE.toDto(findMember, UpdateMemberDto.class);
-        dto.setMemberProfilePublicRange(newSetting.getMemberProfilePublicRange());
-        dto.setAllFeedPublicRange(newSetting.getAllFeedPublicRange());
-        dto.setBuildingSubscriptionPublicRange(newSetting.getBuildingSubscriptionPublicRange());
-        dto.setReceivingAllNotificationAllowed(newSetting.isReceivingAllNotificationAllowed());
+        UpdateMemberDto dto = UpdateMemberDto.builder()
+                .memberId(memberId)
+                .allFeedPublicRange(newSetting.getAllFeedPublicRange())
+                .memberProfilePublicRange(newSetting.getMemberProfilePublicRange())
+                .buildingSubscriptionPublicRange(newSetting.getBuildingSubscriptionPublicRange())
+                .receivingAllNotificationAllowed(newSetting.isReceivingAllNotificationAllowed())
+                .build();
+        log.debug("UpdateMemberDto dto={}", dto);
         this.memberService.updateMember(dto);
     }
 
     @Override
     public Setting findSettingOfMember(String memberId) {
-        Member findMember = this.memberService.findMemberById(memberId);
+        Member findMember = this.memberService.findMemberById(memberId).orElseThrow(NoSuchElementException::new);
         return Setting.builder()
                 .memberProfilePublicRange(findMember.getMemberProfilePublicRange())
                 .allFeedPublicRange(findMember.getAllFeedPublicRange())
                 .buildingSubscriptionPublicRange(findMember.getBuildingSubscriptionPublicRange())
-                .receivingAllNotificationAllowed(findMember.isReceivingAllNotificationAllowed())
+                .receivingAllNotificationAllowed(findMember.getReceivingAllNotificationAllowed())
                 .build();
     }
 }
