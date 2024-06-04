8b3e6dfbf4bf48de6d7698c263a4de9fb1926266
pgd <rudeh1253@gmail.com>
2024-06-04 18:46:46 +0900

    feat: 개발용 NotificationEmptyAgent 구현
    Related to: #41

--- src/main/java/com/kube/noon/config/BeanConfig.java
+++ src/main/java/com/kube/noon/config/BeanConfig.java
@@ -3,6 +3,7 @@ package com.kube.noon.config;
 import com.kube.noon.building.service.buildingwiki.BuildingWikiEmptyServiceImpl;
 import com.kube.noon.building.service.BuildingWikiService;
 import com.kube.noon.notification.service.sender.CoolSmsNotificationAgent;
+import com.kube.noon.notification.service.sender.NotificationEmptyAgent;
 import com.kube.noon.notification.service.sender.NotificationTransmissionAgent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -29,7 +30,14 @@ public class BeanConfig {
     @Value("${cool-sms.from-phone-number}") String fromPhoneNumber;
 
     @Bean
+    @Profile("prod")
     public NotificationTransmissionAgent transmissionAgentForProd() {
         return new CoolSmsNotificationAgent(coolSmsAccessKey, coolSmsSecretKey, fromPhoneNumber);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(NotificationTransmissionAgent.class)
+    public NotificationTransmissionAgent transmissionAgentForDev() {
+        return new NotificationEmptyAgent();
     }
 }
