f195bb3b3f6925bbe6462e051e48500df2482627
pgd <rudeh1253@gmail.com>
2024-06-05 10:42:11 +0900

    refactor: (setting) Service와 외부 레이어 간의 통신 DTO로 변경
    Related to: #54

--- src/main/java/com/kube/noon/setting/dto/SettingDto.java
+++ src/main/java/com/kube/noon/setting/dto/SettingDto.java
@@ -1,4 +1,22 @@
 package com.kube.noon.setting.dto;
 
+import com.kube.noon.common.PublicRange;
+import lombok.*;
+
+/**
+ * 환경설정 DTO
+ *
+ * @author PGD
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class SettingDto {
+    private PublicRange memberProfilePublicRange;
+    private PublicRange allFeedPublicRange;
+    private PublicRange buildingSubscriptionPublicRange;
+    private boolean receivingAllNotificationAllowed;
 }
