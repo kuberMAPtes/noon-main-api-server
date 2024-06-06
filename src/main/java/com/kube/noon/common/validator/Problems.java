624135d7d81c3147933f30b5833d8a5a9a6ceaa9
pgd <rudeh1253@gmail.com>
2024-06-06 16:32:38 +0900

    docs: Problems 객체에 Javadoc 추가
    IllegalServiceCallException의 Javadoc도 약간 수정
    
    Related to: #60

--- src/main/java/com/kube/noon/common/validator/Problems.java
+++ src/main/java/com/kube/noon/common/validator/Problems.java
@@ -5,6 +5,15 @@ import lombok.NoArgsConstructor;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 검증 로직에서 각 문제가 있는 부분을 저장하는 객체
+ * IllegalServiceCallException에 저장된다.
+ * Key에는 문제가 있는 Parameter의 이름이 담길 수 있고
+ * Value에는 아무 객체가 올 수 있으나 보통 String 객체 (검증 실패 메시지)가 담긴다.
+ *
+ * @author PGD
+ * @see IllegalServiceCallException
+ */
 @NoArgsConstructor
 public class Problems extends HashMap<String, Object> {
 
