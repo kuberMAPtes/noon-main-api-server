d661c60c9fa95a21afee8692b5295d5d11256166
pgd <rudeh1253@gmail.com>
2024-06-11 09:18:22 +0900

    feat: UserDetailsService 구현체
    Related to: #87

---
+++ src/main/java/com/kube/noon/common/security/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.kube.noon.common.security.userdetails;
+
+import com.kube.noon.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.memberRepository
+                .findMemberById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Member ID not found: " + username));
+    }
+}
