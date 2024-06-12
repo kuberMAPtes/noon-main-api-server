d0f8b1e1c3a7e3b07c9526bd40ac2c42868ec9bb
jin66 <dbf6609@naver.com>
2024-06-12 14:07:32 +0900

    feat : 채팅방 조회 서비스 추가
    - 건물별 채팅방 목록
    - 건물별 채팅방 목록 검색
    - 전체 채팅방 검색

--- src/main/java/com/kube/noon/chat/repository/ChatroomRepository.java
+++ src/main/java/com/kube/noon/chat/repository/ChatroomRepository.java
@@ -3,8 +3,14 @@ package com.kube.noon.chat.repository;
 import com.kube.noon.chat.domain.Chatroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 public interface ChatroomRepository extends JpaRepository<Chatroom, Integer> {
     // 기본적인 CRUD 메서드는 JpaRepository가 제공
     // 추가적인 커스텀 메서드를 정의할 수 있음
+    List<Chatroom> findByBuildingId(int buildingId);
+    List<Chatroom> findByBuildingIdAndChatroomNameContaining(int buildingId, String searchKeywordChatroom);
+    List<Chatroom> findByChatroomNameContaining(String searchKeywordChatroom);
 
 }
---
+++ src/main/java/com/kube/noon/chat/service/ChatroomSearchService.java
@@ -0,0 +1,47 @@
+package com.kube.noon.chat.service;
+
+import com.kube.noon.chat.dto.ChatroomDto;
+
+import java.util.List;
+
+public interface ChatroomSearchService {
+
+    /**
+     * 건물별 채팅방 목록 조회
+     * @return
+     * @throws Exception
+     */
+    public List<ChatroomDto> getBuildingChatroomList(int buildingId) throws Exception;
+
+    /**
+     * 건물별 채팅방 목록 검색
+     * @param buildingId
+     * @param searchKeywordChatroom
+     * @return
+     * @throws Exception
+     */
+    public List<ChatroomDto> getBuildingChatroomListByChatroomName(int buildingId, String searchKeywordChatroom) throws Exception;
+
+    /**
+     * 내 채팅방 목록 조회 (멤버ID로 채팅방 목록을 조회)
+     * @param chatroomMemberId
+     * @return
+     * @throws Exception
+     */
+    public List<ChatroomDto> getChatroomListByMemberId(String chatroomMemberId) throws Exception;
+
+    /**
+     * 채팅방 이름으로 채팅방 목록 조회 (관리자 기능)
+     * @param chatroomName
+     * @return
+     * @throws Exception
+     */
+    public List<ChatroomDto> getChatroomListByChatroomName(String chatroomName) throws Exception;
+
+    /**
+     * 활발한 채팅방 조회
+     * @return
+     * @throws Exception
+     */
+    public List<ChatroomDto> getLivelistChatroomList() throws Exception;
+}
