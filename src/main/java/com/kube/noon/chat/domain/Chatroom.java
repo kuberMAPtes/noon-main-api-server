73f94a21c11647c41902553f19a69fee1d97a594
pgd <rudeh1253@gmail.com>
2024-06-20 15:37:17 +0900

    refactor: Chatroom에 FK 바인딩된 프로퍼티를 JoinColumn으로
    buildingId -> building
    
    Related to: #131

--- src/main/java/com/kube/noon/chat/domain/Chatroom.java
+++ src/main/java/com/kube/noon/chat/domain/Chatroom.java
@@ -1,10 +1,12 @@
 package com.kube.noon.chat.domain;
 
+import com.kube.noon.building.domain.Building;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "chatroom")
@@ -21,8 +23,11 @@ public class Chatroom {
     @Column(name = "chatroom_creator_id", length = 20, nullable = false)
     private String chatroomCreatorId;
 
-    @Column(name = "building_id", nullable = false)
-    private int buildingId;
+//    @Column(name = "building_id", nullable = false)
+//    private int buildingId;
+    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    @JoinColumn(name = "building_id", referencedColumnName = "building_id", nullable = false)
+    private Building building;
 
     @Column(name = "chatroom_name", length = 50, nullable = false)
     private String chatroomName;
@@ -36,4 +41,7 @@ public class Chatroom {
 
     @Column(name = "chatroom_dajung_temp_min")
     private Float chatroomMinTemp;
+
+    @OneToMany(mappedBy = "chatroom", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    private List<ChatEntrance> chatEntranceList;
 }
