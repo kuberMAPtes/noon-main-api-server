4810ad3683a8a6e7f03dc79aee7e91b448d8c6eb
ellen310 <ellen10735@gmail.com>
2024-06-20 20:41:38 +0900

    feat : get building subscriber list

--- src/main/java/com/kube/noon/building/controller/BuildingProfileRestController.java
+++ src/main/java/com/kube/noon/building/controller/BuildingProfileRestController.java
@@ -89,10 +89,18 @@ public class BuildingProfileRestController {
     }
 
     /**
+     * 건물의 구독자 수 가져오기
+     */
+    @GetMapping("/getSubscriberCnt")
+    public int getSubscriberCnt(@RequestParam("buildingId") int buildingId) {
+        return buildingProfileService.getSubscriberCnt(buildingId);
+    }
+
+    /**
      * 사용자의 화면 범위 내 건물 목록 보기
      */
     @PostMapping("/getBuildingsWithinRange")
-    public List<BuildingDto> getBuildingsWithinRange(@RequestBody PositionRange positionRange){ //////////////////Get으로 받아야되긴 한데.. 파라미터를 사용자측에서 어떤식으로 넘겨줄지 모르겠어서 일단 이렇게 함.
+    public List<BuildingDto> getBuildingsWithinRange(@RequestBody PositionRange positionRange){
 
         return  buildingProfileService.getBuildingsWithinRange(positionRange);
     }
