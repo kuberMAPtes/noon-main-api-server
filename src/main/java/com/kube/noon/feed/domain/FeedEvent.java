f77bde11b9a5fe970118d677887715664de0a85c
WinterHana <haha920@naver.com>
2024-06-27 20:54:19 +0900

    feat : 이벤트 피드 관련 로직 추가

---
+++ src/main/java/com/kube/noon/feed/domain/FeedEvent.java
@@ -0,0 +1,28 @@
+package com.kube.noon.feed.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "feed_event")
+public class FeedEvent {
+
+    @Id
+    @Column(name = "feed_id")
+    private int feedId;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDateTime eventDate;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+}
